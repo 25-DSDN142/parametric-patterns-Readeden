ae510f4c743959a8894ff1ef9aac1b2507651efc
Readeden <edenread18@gmail.com>
2025-08-02 16:17:13 +1200

    Update my_wallpaper.js
    Added more variables.

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,24 +1,30 @@
 //your parameter variables go here!
 var topY = 130 // top of tub
-var wheelX = 170 //wheel center
+var wheelX = 155 //wheel center
 var wheelY = 180 //wheel center
 var handlex = 30 //handle center
 //var handleY = 130 //handle center - I added this to (topY)
 var Btubx = 80 // left bottem of tub
 var BtubY = 170  // left bottem of tub
-let Wheelbarow = false
+var size = 5 //inner flower size
+var leafw = 10 //leaf width
+var leafl = 30  //leaf lenth
+var fully = 200 //lenth
+var fullx = 200 //width
+var rfp = fully-20 // right flower(and leaf) placement
+let Wheelbarow = true //false
 let plants = true //false
 
 function setup_wallpaper(pWallpaper) {
-  //pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
+  //pWallpaper.output_mode(GRID_WALLPAPER);
   
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
-  pWallpaper.grid_settings.cell_width  = 400;
-  pWallpaper.grid_settings.cell_height = 200;
+  pWallpaper.grid_settings.cell_width  = fully;
+  pWallpaper.grid_settings.cell_height = fullx;
   pWallpaper.grid_settings.row_offset  = 0;
 }
 
@@ -33,12 +39,12 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
 if(Wheelbarow){
   // the second wheelbarow
-  strokeWeight(5);
+ /* strokeWeight(5);
  line(230,190,348,190);//bottem handle
  line(230,140,348,140);//top handle
  rect(348,160,5,5);//wheel
  strokeWeight(0.75);
- rect(250,140,100,50); //body
+ rect(250,140,100,50); //body */
 
  //first wheelbarow
  //tub
@@ -64,23 +70,24 @@ if(Wheelbarow){
 
 if(plants){
   strokeWeight();
-  fill(22, 3, 123);//blue flower
-  //flower
- circle(200,100,50)
-
+  //blue flowers
+  fill(22, 3, 123);
+  //big flower
+ //circle(200,50,size)
+ //small flowers
   circle(20,20,10); //top left
-  circle(380,20,20); //top right
-  circle(380,180,10);//bottem right
+  circle(rfp,20,20); //top right
+  circle(rfp,180,10);//bottem right
   circle(20,180,30);//botem left
 
   //leaves
   fill(10, 135, 1);
 
-  ellipse(50, 20, 30, 10);//top left
-  ellipse(20, 50, 10, 30);//left top
-  ellipse(380,50,10,30);//right top
-  ellipse(350,20,30,10) //top right
-  ellipse(380,150,10,30) //bottem right
+  ellipse(50, 20, leafl, leafw);//top left
+  ellipse(20, 50, leafw, leafl);//left top
+  ellipse(rfp,50,leafw,leafl);//right top
+  ellipse(rfp-30,20,leafl,leafw) //top right
+  ellipse(rfp,150,leafw,leafl) //bottem right
 
  //transperent plants
  
